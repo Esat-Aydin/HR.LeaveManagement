290d6de4472f6e96342f3aef0550f10f4d020625
esataydin <esat61_2001@hotmail.com>
2023-12-21 16:45:13 +0100

    xUnit tests done for Features LeaveTypes

--- src/Core/HR.LeaveManagement.Application/Features/LeaveAllocation/Commands/CreateLeaveAllocation/CreateLeaveAllocationCommand.cs
+++
@@ -1,8 +0,0 @@
-﻿using MediatR;
-
-namespace HR.LeaveManagement.Application.Features.LeaveAllocations.Requests.Commands;
-
-public class CreateLeaveAllocationCommand : IRequest<Unit>
-{
-    public int LeaveTypeId { get; set; }
-}
--- src/Core/HR.LeaveManagement.Application/Features/LeaveAllocation/Queries/GetLeaveAllocationDetails/GetLeaveAllocationDetailQuery.cs
+++
@@ -1,8 +0,0 @@
-﻿using MediatR;
-
-namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Queries.GetLeaveAllocationDetails;
-
-public class GetLeaveAllocationDetailQuery : IRequest<LeaveAllocationDetailsDto>
-{
-    public int Id { get; set; }
-}
